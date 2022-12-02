b91175f0a7c0a5ba6b1e44a2d9447aa0318e83ca
Ibnu <ibnunakula99@gmail.com>
2022-12-02 16:05:07 +0800

    test

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -189,7 +189,7 @@
           <img src="<?php echo base_url(); ?>assets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Ale</a>
         </div>
       </div>
 
